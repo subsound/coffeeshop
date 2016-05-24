1aff96ab36b2669e93674105304eb418cc3b4b8f
Nick Balakhtar <midnightcoldlove@gmail.com>
2016-05-24 15:54:55 +0300

    second v

--- client/templates/helpers.js
+++ client/templates/helpers.js
@@ -26,6 +26,58 @@ Template.login.events({
 	}
 });
 
+
+
+/*Template.signin.events({
+
+	'submit .signin-user': function(event){
+		event.preventDefault();
+		var username = event.target.username.value;
+		var password = event.target.password.value;
+
+
+		Meteor.loginWithPassword(username, password, function(err){
+			if(err){
+				event.target.username.value = username;
+				event.target.password.value = password;
+				FlashMessages.sendError(err.reason);
+			} else {
+				FlashMessages.sendSuccess('You are now logged in');
+				Router.go('/admin/projects');
+			}
+		});
+
+	}
+});*/
+
+Template.signin.events({
+    'submit .signin-user': function(event){
+        event.preventDefault();
+        var emailVar = event.target.email.value;
+        var passwordVar = event.target.password.value;
+        var usernameVar = event.target.username.value;
+        console.log("Form submitted.");
+         Accounts.createUser({
+            email: emailVar,
+            password: passwordVar,
+            username: usernameVar
+        });
+    }
+});
+
+Template.signin.isUserAdmin = function(){
+  var adminEmail = Meteor.user().role;
+  if( adminEmail == "admin"){
+    return true;
+  } else {
+    return false;
+    //add some logic for displaying error template.
+    console.log("error");
+  }
+}
+
+
+
 Template.layout.events({
 	'click .logout-user': function(event){
 		Meteor.logout(function(err){
